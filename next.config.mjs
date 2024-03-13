036281d1d3de36c0b3b61fa2830ba3343178e107
cludwell <cludwell@gmail.com>
2024-03-13 16:37:00 -0700

    retry

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Define the export path for static HTML files
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      // Add other routes as needed
+    };
+  },
+};
 
 export default nextConfig;
