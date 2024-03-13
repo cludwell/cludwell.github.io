840d13d7f064dab0299e4e7991a987f8b97e0bdf
cludwell <cludwell@gmail.com>
2024-03-13 16:45:09 -0700

    error messages

--- src/app/components/Skills.jsx
+++ src/app/components/Skills.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import Tab from "./Tab";
 import SideDrawer from "./SideDrawer";
 
-export default function Skills({skillsOpen, setSkillsOpen}) {
+export default function Skills({ skillsOpen, setSkillsOpen }) {
   const skillsRef = useRef(null);
 
   const openDrawer = () => {
@@ -27,14 +27,17 @@ export default function Skills({skillsOpen, setSkillsOpen}) {
   return (
     <>
       <span onClick={openDrawer}>
-        <Tab title={"Skills"} number={"03"} open={skillsOpen}/>
+        <Tab title={"Skills"} number={"03"} open={skillsOpen} />
       </span>
       <SideDrawer open={skillsOpen} setOpen={setSkillsOpen} ref={skillsRef}>
-        <div className="max-w-screen-md  skills">
+        <div className="max-w-screen-md skills">
           <h2 className="text-4xl text-white"> Skills 👨‍💻</h2>
           <p className="my-12">{`At the moment I'm studying C# and also experimenting with frontend frameworks like framer-motion and anime.js`}</p>
           <p href="https://skillicons.dev" align="center">
-            <img src="https://skillicons.dev/icons?i=androidstudio,aws,cs,css,docker,eclipse,express,html,flask,git,js,nodejs,nextjs,postgres,postman,prisma,py,react,redux,sqlite,sequelize,tailwind,ts,visualstudio,vscode&perline=8" />
+            <img
+              src="https://skillicons.dev/icons?i=androidstudio,aws,cs,css,docker,eclipse,express,html,flask,git,js,nodejs,nextjs,postgres,postman,prisma,py,react,redux,sqlite,sequelize,tailwind,ts,visualstudio,vscode&perline=8"
+              alt="a series of technologies that i have used and studied"
+            />
           </p>
         </div>
       </SideDrawer>
