44ab6b5e4dcad6596600a9bb973648b23d252ccd
cludwell <cludwell@gmail.com>
2024-03-13 17:08:29 -0700

    empty image obj

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: ['https://skillicons.dev/']
+    // remotePatterns: ['https://skillicons.dev/']
   },
   output: 'export',
 };
