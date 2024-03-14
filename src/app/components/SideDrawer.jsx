b3f4c67df678c656261209ceb8d20ef39e690a52
cludwell <cludwell@gmail.com>
2024-03-14 01:42:33 -0700

    going to fix landing

--- src/app/components/SideDrawer.jsx
+++ src/app/components/SideDrawer.jsx
@@ -3,12 +3,13 @@ import { useEffect, forwardRef } from "react";
 const SideDrawer = forwardRef(({ open, setOpen, children }, ref) => {
 
   return (
+
     <div
-      className={`
+    className={`
     fixed w-3/4 md:w-2/3 flex transition-all z-30 duration-500 h-screen ease-in-out top-0 bg-black/50 right-0 overflow-y-auto overflow-x-hidden p-2 md:p-6 text-xs sm:text-sm md:text-base
     ${open ? "translate-x-0" : "translate-x-[-100vw] "}
     `}
-      ref={ref}
+    ref={ref}
     >
       <div
         className={`bg-base-200 rounded-xl transition-all duration-500 mx-4 ease-in-out text-white leading-6 md:leading-8 my-auto
