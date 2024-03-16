be5010c46a1735793f967183b83bafbacd897e5d
cludwell <cludwell@gmail.com>
2024-03-16 00:43:12 -0700

    removed red background

--- src/app/components/Tab.jsx
+++ src/app/components/Tab.jsx
@@ -1,7 +1,7 @@
 import { gloock, bebas } from "../fonts";
 export default function Tab({ title, number, open }) {
   return (
-    <li className={`relative flex flex-col justify-center py-6 sm:my-4 md:my-8 text-white transition-all duration-200 ease-in-out cursor-pointer hover:translate-x-8 hover:text-orange-600 tab scale-[.6] sm:scale-75 md:scale-100 bg-red-400 ${
+    <li className={`relative flex flex-col justify-center py-6 sm:my-4 md:my-8 text-white transition-all duration-200 ease-in-out cursor-pointer hover:translate-x-8 hover:text-orange-600 tab scale-[.6] sm:scale-75 md:scale-100  ${
       open ? " text-orange-600 translate-x-8 " : ''
     }`} >
       <div
